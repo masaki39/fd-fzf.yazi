faaa1e5f3dfa6249b9ad0e66e70ba83fbd738736
masaki39 <masa21gifus@gmail.com>
2026-02-27 08:37:19 +0900

    realtime search

--- main.lua
+++ main.lua
@@ -7,28 +7,21 @@ end)
 function M:entry()
 	local cwd = state()
 
-	local fd_output, _ = Command("fd")
-		:arg({ "--type", "d" })
+	local permit = ui.hide()
+	local child, spawn_err = Command("sh")
+		:arg("-c")
+		:arg("fd --type d | fzf")
 		:cwd(tostring(cwd))
 		:stdout(Command.PIPED)
-		:output()
-	if not fd_output or fd_output.stdout == "" then
-		ya.notify({ title = "fd-fzf.yazi", content = "No directories found", level = "warn", timeout = 3 })
-		return
-	end
-
-	local permit = ui.hide()
-	local child, spawn_err =
-		Command("fzf"):stdin(Command.PIPED):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
+		:stderr(Command.INHERIT)
+		:spawn()
 
 	if not child then
 		permit:drop()
-		ya.notify({ title = "fd-fzf.yazi", content = "fzf failed: " .. tostring(spawn_err), level = "error", timeout = 3 })
+		ya.notify({ title = "fd-fzf.yazi", content = "Failed: " .. tostring(spawn_err), level = "error", timeout = 3 })
 		return
 	end
 
-	child:write_all(fd_output.stdout)
-	child:flush()
 	local output, err = child:wait_with_output()
 	permit:drop()
 
